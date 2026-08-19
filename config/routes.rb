882c2aeed01578e775263952c6355fc43a7c7665
oyamairishitomi <h.tezuka0707@gmail.com>
2026-08-19 14:52:35 +0900

    OGP/Twitter Card対応とプライバシーポリシー・利用規約ページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
     end
   end
 
+  get "privacy" => "static_pages#privacy"
+  get "terms" => "static_pages#terms"
+
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
