84424fc1dc82be22f7519fdc4f035977570cc95e
oyamairishitomi <h.tezuka0707@gmail.com>
2026-08-19 17:28:41 +0900

    passwordバリデーションのallow_nilを修正(既存レコード更新時に誤って弾かれる不具合)

--- app/models/family.rb
+++ app/models/family.rb
@@ -2,7 +2,7 @@ class Family < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :aikotoba, presence: true, uniqueness: true
-  validates :password, length: { minimum: 8 }, allow_nil: false
+  validates :password, length: { minimum: 8 }, allow_nil: true
   validates :privacy_agreement, acceptance: true
   validates :terms_agreement, acceptance: true
 
