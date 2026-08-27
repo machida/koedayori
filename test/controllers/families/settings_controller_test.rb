7d701e7636ca3e050602a12735dc3b82e5e77688
oyamairishitomi <h.tezuka0707@gmail.com>
2026-08-27 14:32:25 +0900

    rubocopの指摘(スペース)を自動修正

--- test/controllers/families/settings_controller_test.rb
+++ test/controllers/families/settings_controller_test.rb
@@ -3,18 +3,18 @@ require "test_helper"
 class Families::SettingsControllerTest < ActionDispatch::IntegrationTest
   test "メールアドレスの更新に成功する" do
     family = Family.create!(email: "a@test.com", aikotoba: "aaa", password: "password123")
-    post families_sessions_path, params: {family: { aikotoba: family.aikotoba, password: "password123" } }
+    post families_sessions_path, params: { family: { aikotoba: family.aikotoba, password: "password123" } }
 
-    patch families_settings_path, params: {email: "b@test.com", aikotoba: family.aikotoba}
+    patch families_settings_path, params: { email: "b@test.com", aikotoba: family.aikotoba }
     family.reload
     assert_equal "b@test.com", family.email
   end
 
   test "あいことばの更新に成功する" do
     family = Family.create!(email: "a@test.com", aikotoba: "aaa", password: "password123")
-    post families_sessions_path, params: {family: { aikotoba: family.aikotoba, password: "password123" } }
+    post families_sessions_path, params: { family: { aikotoba: family.aikotoba, password: "password123" } }
 
-    patch families_settings_path, params: {email: family.email, aikotoba: "bbb"}
+    patch families_settings_path, params: { email: family.email, aikotoba: "bbb" }
     family.reload
     assert_equal "bbb", family.aikotoba
   end
